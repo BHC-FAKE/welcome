c38bf1bce0dbabb7f1105272e6d71e52ecaa85ab
BHC-3RR0R <51839125+BHC-FAKE@users.noreply.github.com>
2019-06-21 20:18:01 +0700

    aowkwkwk

---
+++ run.sh
@@ -0,0 +1,97 @@
+#usr/bin/bash#
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+clear
+echo
+echo
+figlet INSTALL DOLO TUNGGU | lolcat
+clear
+echo
+apt update && apt upgrade
+apt install nano
+apt install git
+pkg install python python2 vim figlet curl
+apt install php
+pip2 install mechanize
+pip2 install lolcat
+pip2 install requests
+echo
+clear
+figlet WELCOME MY TOOLS😙😙😙 | lolcat
+echo $i"["$bi"•"$i"]"$me"────────────────────────────────────────────────────────────────"$i"["$bi"•"$i"]"
+echo $i" |"$cy"                        BLACK HAT CYBER TEAM"$i"                      |"
+echo $i" |"$me"──────────────────────────────────────────────────────────────────"$i"|"
+echo $i" |"$pu" AUTHOR :"$ku" FAKE  [BlackHatCyber]"$i"                                   |"
+echo $i" |"$pu" PLANET :"$ku" MARS"$i"                                                    |"
+echo $i" |"$pu" THANKS TO :"$cy"~ [BHC•O²N¦Mr.Rexx¦√] [BHC•Mr.HanSIPᵀᴶᴳ] [BHC•Poker]"$i"  |"
+echo $i" |"$cy"            ~ [BHC•Crazy] [BHC•Hackeyts] [༆B.H.C•FRI❸NDS࿐]"$i"        |"
+echo $i" |"$pu" Contack WhatsApp :"$ku" 0858××××××××"$i"                                  |"
+echo $i" |"$pu" Contack Gmail :"$ku" blackhatcyberteam@gmail.com"$i"                      |"
+echo $i"["$bi"•"$i"]"$me"────────────────────────────────────────────────────────────────"$i"["$bi"•"$i"]"
+echo
+echo $i"────────────────────────────────    ─────"
+echo $i"|"$me" 1"$i" |"$cy" TOOLS BRUTAL SMS WORK    "$i"|"$i"    |"$cy" J"$i" |"
+echo $i"|"$me" 2"$i" |"$cy" TOOLS PENAMBAH PINTAS    "$i"|"$i"    |"$cy" A"$i" |"
+echo $i"|"$me" 3"$i" |"$cy" TOOLS YAHOO CLONING      "$i"|"$i"    |"$cy" H"$i" |"
+echo $i"|"$me" 4"$i" |"$cy" TOOLS BUAT DEFACER       "$i"|"$i"    |"$cy" A"$i" |"
+echo $i"|"$me" 5"$i" |"$cy" TOOLS DARK-FB            "$i"|"$i"    |"$cy" T"$i" |"
+echo $i"────────────────────────────────"$i
+echo $me"┌==="$bi"["$i"BlackHatCyber"$bi"]"$me"======"$bi"["$i""Pilih Nomornya""$bi"]"
+echo $me"¦"
+read -p"└──# " pil
+
+if [ $pil = 1 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/TERMUXID3/brutal-sms
+cd brutal-sms
+python2 run.py
+fi
+
+if [ $pil = 2 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/BHC-FAKE/keyrun1
+cd keyrun1
+python2 runcut.py
+fi
+
+if [ $pil = 3 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/EmailVuln
+cd EmailVuln
+python2 vuln.py
+
+fi
+if [ $pil = 4 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/404rgr/webdav77
+cd webdav77
+bash webdav77.sh
+fi
+
+if [ $pil = 5 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/pashayogi/SETAN
+cd SETAN
+python2 SETAN.py
+fi
